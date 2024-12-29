739a343c36a3913eda72cf9e3d45367f795333ca
Matthias Tylkowski <matthias@tylkowski.net>
2024-12-29 10:00:03 +0100

    task: basic reproduction

--- test/_community/collections/Media/index.ts
+++ test/_community/collections/Media/index.ts
@@ -8,7 +8,60 @@ export const MediaCollection: CollectionConfig = {
     create: () => true,
     read: () => true,
   },
-  fields: [],
+  fields: [
+    {
+      name: 'noAccess',
+      type: 'text',
+      access: {
+        read: () => false,
+        create: () => false,
+        update: () => false,
+      },
+    },
+    {
+      name: 'noCondition',
+      type: 'text',
+      admin: {
+        condition: () => false,
+      },
+    },
+    {
+      name: 'sizes',
+      type: 'group',
+      fields: [
+        {
+          name: 'thumbnail',
+          type: 'group',
+          fields: [
+            {
+              name: 'mimeType',
+              type: 'text',
+              admin: {
+                disableListColumn: true,
+                disableListFilter: true,
+              },
+            },
+          ],
+          admin: {
+            disableListColumn: true,
+            disableListFilter: true,
+          },
+        },
+      ],
+      admin: {
+        disableListColumn: true,
+        disableListFilter: true,
+      },
+    },
+    {
+      name: 'mimeType',
+      type: 'text',
+      admin: {
+        disableListColumn: true,
+        disableListFilter: true,
+      },
+    },
+  ],
   upload: {
     crop: true,
     focalPoint: true,
